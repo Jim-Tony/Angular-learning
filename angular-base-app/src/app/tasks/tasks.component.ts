3bc0893ea0d4a60f8b28214b16dd2c9ab4a92700
Jim Tony <jim.tony@navis.com>
2024-12-01 15:36:48 +0530

    Added a skeletol new-task dialog

--- angular-base-app/src/app/tasks/tasks.component.ts
+++ angular-base-app/src/app/tasks/tasks.component.ts
@@ -1,18 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
 import { dummyTasks } from './dummy-tasks';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent,NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required:true}) userName?:string;
   @Input({required:true}) id?:string;
+  isTaskNeedToAdd = false;
   tasks = dummyTasks;
+
   onCompleteTask(id:string){
     this.tasks = this.tasks.filter(t=>t.id!==id);
+  }
+  
+  onAddTask() {
+    this.isTaskNeedToAdd = true;
   }
 }
